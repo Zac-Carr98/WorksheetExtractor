8c7e12260b8aa7dfb5f92fbd8d3d0f986d9a98c9
Zac Carr <zac.carr98@gmail.com>
2021-05-19 16:14:54 -0500

    cleaned mainwindow

--- main_window.py
+++ main_window.py
@@ -25,37 +25,44 @@ class MainWindow:
 
         # label to push items down
         self.placement = tk.Label(self.root, bg='white')
-        self.placement.pack(pady=100)
 
         # btn_select
         # self.btn_select = tk.Button(self.root, text='Click to Select File', command=self.open_dialog,
         # bg='#ff7f27', fg='black')
         self.btn_select = CustomButton(self.root, text='Click to Select File', command=self.open_dialog)
+
         # self.loadimage = tk.PhotoImage(file="C:/Users/tpsharp/Pictures/Saved Pictures/SaveLocation.png")
         # self.btn_select = tk.Button(self.root, image=self.loadimage)
         # self.btn_select["bg"] = "white"
         # self.btn_select["border"] = "0"
-        self.btn_select.pack(pady=10)
 
         # btn location
         self.btn_location = CustomButton(self.root, text='Save Location', command=self.save_location)
-        self.btn_location.pack(pady=10)
 
         # Frame for keyword entry
         self.frame1 = tk.Frame(root, bg='white')
-        self.frame1.pack(pady=10)
 
         # text entry
         self.text_entry = tk.Entry(self.frame1)
-        self.text_entry.grid(row=0, column=0, padx=10)
 
         # submit keywords
         self.submit_keyword = CustomButton(self.frame1, text='Submit', command=self.add_keyword)
-        self.submit_keyword.grid(row=0, column=1, padx=10, sticky=tk.W)
 
         # btn_run
         self.btn_run = CustomButton(self.root, text='Run', command=self.run)
+
+
+    def grid(self):
+        self.placement.pack(pady=100)
+        self.btn_select.pack(pady=10)
+        self.btn_location.pack(pady=10)
+
+        self.frame1.pack(pady=10)
+        self.text_entry.grid(row=0, column=0, padx=10)
+        self.submit_keyword.grid(row=0, column=1, padx=10, sticky=tk.W)
+
         self.btn_run.pack(pady=10)
+
 
     def open_dialog(self):
         self.backend.select_target_file(fd.askopenfilename())
